c3d461f6b46448cdad324222c4eda3e1df244d5f
Patternscompany <pandureddypatterns@gmail.com>
2024-09-02 20:46:42 +0530

    pp

--- js/main.js
+++ js/main.js
@@ -104,6 +104,30 @@
         },
       },
     });
+    $(".services-carousel").owlCarousel({
+      autoplay: true,
+      center: true,
+      loop: true,
+      items: 1,
+      margin: 30,
+      stagePadding: 0,
+      nav: false,
+      navText: [
+        '<span class="ion-ios-arrow-back">',
+        '<span class="ion-ios-arrow-forward">',
+      ],
+      responsive: {
+        0: {
+          items: 1,
+        },
+        600: {
+          items: 2,
+        },
+        1000: {
+          items: 3,
+        },
+      },
+    });
     $(".home").owlCarousel({
       autoplay: true,
       center: true,
